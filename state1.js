3b61b63c0aadfd68b34ebe65b7b6a48a378e343c
Nicholas Hoang <nicholash489@gmail.com>
2021-02-28 21:34:25 -0600

    added hunter sprite

--- state1.js
+++ state1.js
@@ -1,9 +1,11 @@
 var player, keys, speed = 200;
+var enemy;
 
 demo.state1 = function(){};
 demo.state1.prototype = {
     preload: function(){
         game.load.spritesheet("seal", "assets/sprites/HarpSeal.png", 160, 160);
+        game.load.spritesheet("hunter", "assets/sprites/hunter.png", 64, 64);
         game.load.tilemap("mapTest", "assets/tilemaps/demomap.json", null, Phaser.Tilemap.TILED_JSON);
         game.load.image("Ground", "assets/tilemaps/set_01.png");
     },
@@ -18,7 +20,7 @@ demo.state1.prototype = {
         map.addTilesetImage("Ground");
         bounds = map.createLayer("Background");
 
-        player = game.add.sprite(game.world.centerX, game.world.centerY, "seal");
+        player = game.add.sprite(200, game.world.centerY, "seal");
         player.anchor.setTo(0.5, 0.5);
         player.scale.setTo(-0.6, 0.6)
         game.physics.enable(player);
@@ -26,12 +28,21 @@ demo.state1.prototype = {
         game.camera.follow(player);
         player.animations.add("walk", [0, 1, 2]);
 
+        enemy = game.add.sprite(600, game.world.centerY, "hunter");
+        enemy.frame = 1;
+        enemy.anchor.setTo(0.5, 0.5);
+        enemy.scale.setTo(2, 2);
+        game.physics.enable(enemy);
+        enemy.body.immovable = true;
+        enemy.body.collideWorldBounds = true;
+
         keys = game.input.keyboard.addKeys({
             "up": 87, "down": 83, "left": 65, "right": 68
         });
     },
 
     update: function (){
+        game.physics.arcade.collide(player, enemy);
         if(keys.up.isDown){
             player.body.velocity.y = -speed;
             player.animations.play("walk", 14, true);
