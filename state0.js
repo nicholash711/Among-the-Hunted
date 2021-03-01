e9a5e82a1101279c6fb34ba23d6051cb99c30863
Marianne Le <63270221+iridazzle@users.noreply.github.com>
2021-02-28 21:31:16 -0600

    Update state0.js

--- state0.js
+++ state0.js
@@ -3,18 +3,19 @@ var demo = {};
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function(){
-        game.load.atlasJSONHash('seal', 'assets/sprites/harp_seal_shooting.png', 'assets/sprites/harp_seal_shooting.json');
+        //game.load.atlasJSONHash('seal', 'assets/sprites/harp_seal_shooting.png', 'assets/sprites/harp_seal_shooting.json');
+        game.load.spritesheet('seal', 'assets/sprites/harp_seal_shooting.png', 348, 216, 73)
     },
 
     create: function(){
         game.stage.backgroundColor = '#b5e7ff';
         game.add.text(50, 50, 'Among The Hunted', { fontSize: '32px'});
-        game.add.text(50, 70, 'You are a harp seal\njust abandoned by your mother,\nleft to fend for yourself.\nYou are being hunted\nfor your soft fur.\nTry your best to survive.')
-        console.log('Welcome Haha');
+        game.add.text(50, 75, 'You are a harp seal\njust abandoned by your mother,\nleft to fend for yourself.\nYou are being hunted\nfor your soft fur.\nTry your best to survive.', { fontsize: '24px'})
+        console.log('Welcome Haha',);
         var seal = game.add.sprite(game.world.centerX, 400, 'seal');
         seal.scale.setTo(0.5, 0.5);
         seal.animations.add('shoot');
-        seal.animations.play('shoot', 15, true);
+        seal.animations.play('shoot', 10, true);
         game.input.onDown.add(actionOnClick);
     },
 
