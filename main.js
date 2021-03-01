1dc626532063bdddb6b3943d4649458ddffcd92e
Nicholas Hoang <nicholash489@gmail.com>
2021-02-28 20:30:36 -0600

    tilemap and seal loaded

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const WIDTH = 1500, HEIGHT = 1000;
+const WIDTH = 800, HEIGHT = 600;
 
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO);
 game.state.add("state0", demo.state0);
 game.state.add("state1", demo.state1);
-game.state.start("state0");
+game.state.start("state1");
