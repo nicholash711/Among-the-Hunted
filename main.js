33cb7c0baef512b45979352eee24749eb05dca01
Nicholas Hoang <nicholash489@gmail.com>
2021-02-28 20:43:38 -0600

    changes

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ const WIDTH = 800, HEIGHT = 600;
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO);
 game.state.add("state0", demo.state0);
 game.state.add("state1", demo.state1);
-game.state.start("state1");
+game.state.start("state0");
