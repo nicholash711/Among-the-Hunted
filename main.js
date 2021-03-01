180557af4f349cea38420cc46b7c44774cd67e36
Nicholas Hoang <nicholash489@gmail.com>
2021-02-28 19:05:17 -0600

    conventions updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const width = 1500, height = 1000;
+const WIDTH = 1500, HEIGHT = 1000;
 
-var game = new Phaser.Game(width, height, Phaser.AUTO);
+var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO);
 game.state.add("state0", demo.state0);
 game.state.add("state1", demo.state1);
 game.state.start("state0");
