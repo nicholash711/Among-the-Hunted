18b182dc166e9d1a6f1350425bdcbf957ef80bda
Irida <iridazzle@gmail.com>
2021-03-01 15:57:02 -0600

    added comment for size of browser window

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const WIDTH = 640, HEIGHT = 480;
 
+//const WIDTH = window.innerWidth * window.devicePixelRatio, HEIGHT = window.innerHeight * window.devicePixelRatio
+
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO);
 game.state.add("state0", demo.state0);
 game.state.add("state1", demo.state1);
