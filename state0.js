1b3770f5b6a1eeff0d566181a4920f29e2293adb
Irida <iridazzle@gmail.com>
2021-02-28 21:49:04 -0600

    added instructions to click on seal

--- state0.js
+++ state0.js
@@ -9,7 +9,8 @@ demo.state0.prototype = {
     create: function(){
         game.stage.backgroundColor = '#b5e7ff';
         game.add.text(50, 50, 'Among The Hunted', { fontSize: '32px'});
-        game.add.text(50, 75, 'You are a harp seal\njust abandoned by your mother,\nleft to fend for yourself.\nYou are being hunted\nfor your soft fur.\nTry your best to survive.', { fontsize: '24px'})
+        game.add.text(50, 77, 'You are a harp seal\njust abandoned by your mother,\nleft to fend for yourself.\nYou are being hunted\nfor your soft fur.\nTry your best to survive.', { fontsize: '24px'})
+        game.add.text(50, 300, 'Click on the GIF to begin', { fontSize: '18px'})
         console.log('Welcome Haha',);
         var seal = game.add.sprite(game.world.centerX, 350, 'seal');
         seal.scale.setTo(0.5, 0.5);
