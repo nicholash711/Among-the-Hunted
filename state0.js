e02bd2ba970a8656224bb77615ee73fe36ddbbe9
Nicholas Hoang <nicholash489@gmail.com>
2021-02-28 21:36:10 -0600

    button pos fix

--- state0.js
+++ state0.js
@@ -11,7 +11,7 @@ demo.state0.prototype = {
         game.add.text(50, 50, 'Among The Hunted', { fontSize: '32px'});
         game.add.text(50, 75, 'You are a harp seal\njust abandoned by your mother,\nleft to fend for yourself.\nYou are being hunted\nfor your soft fur.\nTry your best to survive.', { fontsize: '24px'})
         console.log('Welcome Haha',);
-        var seal = game.add.sprite(game.world.centerX, 400, 'seal');
+        var seal = game.add.sprite(game.world.centerX, 350, 'seal');
         seal.scale.setTo(0.5, 0.5);
         seal.animations.add('shoot');
         seal.animations.play('shoot', 10, true);
