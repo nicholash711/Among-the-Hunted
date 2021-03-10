66e78f893ea74483c0a8c603f9e526839cd3a3d7
Nicholas Hoang <nicholash489@gmail.com>
2021-03-10 15:42:55 -0600

    state2 access

--- state2.js
+++ state2.js
@@ -1,8 +1,13 @@
+var text;
+
 demo.state2 = function(){};
 demo.state2.prototype = {
     preload: function(){},
 
-    create: function(){},
+    create: function(){
+        text = game.add.text(WIDTH / 2, 100, "GAME OVER", { fontSize: "40px" });
+        //text.anchor = (0.5, 0.5);
+    },
     
     update: function(){}
 }
